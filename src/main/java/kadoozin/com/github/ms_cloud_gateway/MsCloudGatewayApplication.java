36e84ad70297fb5edac1763791948e60cfb8c64e
kadoozin <kadoozin@gmail.com>
2026-05-23 08:36:56 -0300

    first commit

---
+++ src/main/java/kadoozin/com/github/ms_cloud_gateway/MsCloudGatewayApplication.java
@@ -0,0 +1,26 @@
+package kadoozin.com.github.ms_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator createRouteLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
+				.build();
+	}
+
+}
---
+++ src/test/java/kadoozin/com/github/ms_cloud_gateway/MsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kadoozin.com.github.ms_cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
